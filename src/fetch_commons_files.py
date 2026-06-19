2284b26347147b1042974fcd3b393628907817bf
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-06-20 02:58:09 +0300

    0

--- src/fetch_commons_files.py
+++ src/fetch_commons_files.py
@@ -13,7 +13,7 @@ Requirements:
 import logging
 
 from main_app.main_fetch_files import fetch_files_entry
-from main_app.owid_config import LOG_FILE_COUNTRIES
+from main_app.owid_config import LOG_FILE
 from main_app.logger_config import setup_logging
 
 logger = logging.getLogger(__name__)
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 setup_logging(
     level="INFO",
     name="main_app",
-    log_file=str(LOG_FILE_COUNTRIES),
+    log_file=str(LOG_FILE),
     use_colorlog=False,
     overwrite=True,
 )
--- src/run_categorize.py
+++ src/run_categorize.py
@@ -26,7 +26,7 @@ import argparse
 import logging
 
 from main_app.main_run_categorize import run_categories_entry
-from main_app.owid_config import LOG_FILE_CONTINENTS
+from main_app.owid_config import LOG_FILE_CONTINENTS, LOG_FILE_COUNTRIES
 from main_app.logger_config import setup_logging
 
 logger = logging.getLogger(__name__)
